914e18dc90a43c886de08fd89ae73a39c5318e2b
Abu Ali Aftabuddin Mondal <79456267+Mraftabali963@users.noreply.github.com>
2026-07-31 11:29:12 +0530

    Feat: Count Badge, Websocket

--- src/config/app-config.service.ts
+++ src/config/app-config.service.ts
@@ -8,28 +8,28 @@ class AppConfigService {
   constructor() {
     /*app configurations*/
     this.envConfig[AppConfig.APP] = {
-      port: parseInt(process.env.APP_PORT, 10) || 8000,
-      environment: process.env.ENVIRONMENT || 'development',
-      uiUrl: process.env.UI_URL || 'http://localhost:5173'
+      port: parseInt(process.env.APP_PORT, 10),
+      environment: process.env.ENVIRONMENT,
+      uiUrl: process.env.UI_URL
     };
 
     /*database*/
     this.envConfig[AppConfig.DB] = {
       mongo: {
         uri: process.env.MONGO_CONNECTION_STRING,
-        isSync: parseInt(process.env.MONGO_SYNC_BIT, 10) || 1
+        isSync: parseInt(process.env.MONGO_SYNC_BIT, 10)
       }
     };
 
     /*logger*/
     this.envConfig[AppConfig.LOGGER] = {
-      logLevel: process.env.LOG_LEVEL || 'debug'
+      logLevel: process.env.LOG_LEVEL
     };
 
     /*jwt*/
     this.envConfig[AppConfig.JWT] = {
-      accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET || 'super-secret-access-token-key-change-me',
-      accessTokenExpiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN || '1d'
+      accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+      accessTokenExpiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN
     };
 
     this.envConfig[AppConfig.AES_KEY]={
